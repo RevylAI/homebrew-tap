6fd7fb3f9420002bfbe05252f52035ce6c8297a3
Landseer100 <revyldemo@gmail.com>
2026-02-16 23:46:49 -0800

    Update revyl to v0.1.2

--- Formula/revyl.rb
+++ Formula/revyl.rb
@@ -1,26 +1,26 @@
 class Revyl < Formula
   desc "AI-powered mobile app testing CLI"
   homepage "https://revyl.ai"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/RevylAI/revyl-cli/releases/download/v0.1.1/revyl-darwin-arm64"
-      sha256 "ab601bbd2aa1240ae28b3ecc3df543ef382c242afbd82fc909bf4d973c370170"
+      url "https://github.com/RevylAI/revyl-cli/releases/download/v0.1.2/revyl-darwin-arm64"
+      sha256 "341ee5fe0a1ba5fc79c41a4ccd083f66c85a205ffd97f8420796ca995ee9bcec"
     else
-      url "https://github.com/RevylAI/revyl-cli/releases/download/v0.1.1/revyl-darwin-amd64"
-      sha256 "38cfb49f8a7e2c5c342eb24975e45362dc281698cb905086a71091bf4bd79e9c"
+      url "https://github.com/RevylAI/revyl-cli/releases/download/v0.1.2/revyl-darwin-amd64"
+      sha256 "b473d6d79dba1be6660a153cb97fc5bd13e42b5678e55f08ee0b978dbd7aaeee"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/RevylAI/revyl-cli/releases/download/v0.1.1/revyl-linux-arm64"
-      sha256 "3e14752f62acaef73069362767fb3c636b353993004cab26a7cbbffecc4b53dc"
+      url "https://github.com/RevylAI/revyl-cli/releases/download/v0.1.2/revyl-linux-arm64"
+      sha256 "484e6cf6f84bba2bd7a6e8006423d3f659e4b324c4c9724c4bf842543df0d8ae"
     else
-      url "https://github.com/RevylAI/revyl-cli/releases/download/v0.1.1/revyl-linux-amd64"
-      sha256 "4e126dfd284785fda11862cda3bc48323aa14aff424df169dceaefeacc714cfb"
+      url "https://github.com/RevylAI/revyl-cli/releases/download/v0.1.2/revyl-linux-amd64"
+      sha256 "374c9deb35096e343e79964ca9ad52d9664a7de10b72a4fd186094b84e0f54a1"
     end
   end
 
